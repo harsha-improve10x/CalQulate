e9862f8319a870948cfa7e8872689243ef7cb736
harsha-improve10X <shreeharsha@improve10x.com>
2024-05-10 17:31:19 +0530

    Created a project and pushed to github

---
+++ app/src/main/java/com/example/calqulate/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.example.calqulate
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.calqulate.ui.theme.CalQulateTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            CalQulateTheme {
+
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    CalQulateTheme {
+
+    }
+}
